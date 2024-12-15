e84782e2683b29e7ac8032588ac74389c0a71d96
dolvin17 <huertas876@gmail.com>
2024-12-15 03:39:40 +0100

    adding a fn to shortened eth address on the frontend

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge";
 export function cn(...args: ClassValue[]) {
   return twMerge(clsx(args));
 }
+
+export const beautifyAddress = (addr: string, size = 4, separator = "...") =>
+  `${addr.substr(0, size)}${separator}${addr.substr(-size, size)}`;
