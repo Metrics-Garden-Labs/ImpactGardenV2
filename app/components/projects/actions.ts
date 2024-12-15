321301a5aac34949504bb23017f8a72bb0de49b1
dolvin17 <huertas876@gmail.com>
2024-12-15 03:47:10 +0100

    fix: improve address validation and add user creation logic

--- app/components/projects/actions.ts
+++ app/components/projects/actions.ts
@@ -1,21 +1,37 @@
 "use server";
 
 import { db } from "../../../src/lib/db/dbattestations";
-import { user_addresses } from "../../../src/lib/schema";
+import { user_addresses, users } from "../../../src/lib/schema";
 import { eq } from "drizzle-orm";
 import { NEXT_PUBLIC_URL } from "@/src/config/config";
 
-export async function getFIDbyAddress(address: string) {
+const existAddressData = async (address: string) => {
   const result = await db
     .select()
     .from(user_addresses)
     .limit(1)
     .where(eq(user_addresses.ethaddress, address.toLowerCase()));
 
-  const dbUserID = result?.[0]?.userfid;
+  return (result?.[0] || null) as (typeof result)[0] | null;
+};
+
+import { createHash } from "crypto";
+
+function generateCode(address: string): string {
+  const hash = createHash("sha256").update(address).digest("hex");
+  const code = parseInt(hash.slice(0, 8), 16) % 100000000;
+  return code.toString().padEnd(8, "0");
+}
+
+export async function getFIDbyAddress(address: string) {
+  const result = await existAddressData(address);
+  // Check if the user already exists in the database
+
+  const dbUserID = result?.userfid;
   if (dbUserID) return dbUserID;
 
   try {
+    // If the user doesn't exist, insert the user into the database
     const options = {
       method: "GET",
       headers: {
@@ -31,12 +47,8 @@ export async function getFIDbyAddress(address: string) {
     );
 
     const data = await response.json();
-    console.debug({ data });
-
     const apiData = Object.values(data)?.[0] as [{ fid: number }];
     const apiFID = apiData?.[0]?.fid;
-
-    console.debug({ apiData, apiFID });
 
     if (apiFID) {
       await fetch(`${NEXT_PUBLIC_URL}/api/login`, {
@@ -46,12 +58,27 @@ export async function getFIDbyAddress(address: string) {
         },
         body: JSON.stringify({ fid: apiFID }),
       });
+
+      return apiFID;
     }
   } catch (_) {}
 
-  // if (fid) return fid;
+  // We check again if the user exists in the database
+  const dataExists = await existAddressData(address);
+  if (dataExists) return dataExists.userfid;
 
   // If the user doesn't exist, insert the user into the database
   // Create a unique identifier for the user (fid)
-  return null;
+  const addressFID = generateCode(address);
+
+  const newUser = {
+    fid: addressFID,
+    username: address,
+    pfp_url: "/pfp.png",
+  };
+
+  console.log("New User, ", newUser);
+  await db.insert(users).values(newUser);
+
+  return addressFID;
 }
